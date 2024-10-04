17a8775667fe0286a8d13a6d72e0606683cca91a
maxkauf-hash <77574291+maxkauf-hash@users.noreply.github.com>
2024-10-04 14:08:37 +0200

    ajout config firebase

---
+++ config/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB51wZrq4gv-z8flGJqxvHH6vVAlFZR7TM",
+  authDomain: "nextjstest-679a8.firebaseapp.com",
+  projectId: "nextjstest-679a8",
+  storageBucket: "nextjstest-679a8.appspot.com",
+  messagingSenderId: "194546687532",
+  appId: "1:194546687532:web:9abfe0917d9f88f8cf7b89",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
